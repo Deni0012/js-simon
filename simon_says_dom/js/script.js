4b4bc78b38ce6a7c12928cf52fa6a3dba0a5b16e
Deni0012 <deni.dg00@icloud.com>
2025-03-06 18:48:39 +0100

    feat: elementi html

---
+++ simon_says_dom/js/script.js
@@ -0,0 +1,9 @@
+// Elementi Html 
+const countdownElement = document.getElementById("countdown");
+const numbersListElement = document.getElementById("numbers-list");
+const answersFormElement = document.getElementById("answers-form");
+const buttonElement = document.querySelector("button");
+const messageElement = document.getElementById("message");
+const instructionsElement = document.getElementById("instructions")
+
+const numbersListArr = [];
